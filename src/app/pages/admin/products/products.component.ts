49583fe0e7cd442f33c1a815cf37528e6ef1169b
SachiniPinnaduwa <sachinipinnaduwa2@gmail.com>
2024-02-29 15:51:46 +0530

    commit1

---
+++ src/app/pages/admin/products/products.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/products/product.service';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css',
+})
+export class ProductsComponent implements OnInit {
+  isSidePanelVisiible: boolean = false;
+  productObj: any = {
+    productId: 0,
+    productSku: '',
+    productName: '',
+    productPrice: 0,
+    productShortName: '',
+    productDescription: '',
+    createdDate: new Date(),
+    deliveryTimeSpan: '',
+    categoryId: 0,
+    productImageUrl: '',
+  };
+  categoryList: any[] = [];
+
+  constructor(private productSrv: ProductService) {}
+
+  ngOnInit(): void {
+    this.getAllCategory();
+  }
+
+  getAllCategory() {
+    this.productSrv.getCategory().subscribe((res: any) => {
+      this.categoryList = res.data;
+    });
+  }
+
+  openSidePanel() {
+    this.isSidePanelVisiible = true;
+  }
+  closeSidePanel() {
+    this.isSidePanelVisiible = false;
+  }
+}
